cd847389cd99a6ddc375f815d840d7a083838043
mariuste <mariustetard@googlemail.com>
2022-08-12 21:04:39 +0200

    adopted more sophisticated register setup from different test

--- SW/LCD_Timer_Clock/Core/Inc/SX1503.h
+++ SW/LCD_Timer_Clock/Core/Inc/SX1503.h
@@ -18,22 +18,43 @@
 #include "stm32g0xx_hal.h"
 
 
-// Port Expander Test
+// Port Expander
 static const uint8_t SX1503_ADDR = 0x20 << 1; // Use 8-bit address
-static const uint8_t SX_1503_RegDataB = 0x00; // register address
-static const uint8_t SX_1503_RegDataA = 0x01; // register address
-static const uint8_t SX_1503_RegDirB = 0x02; // register address
-static const uint8_t SX_1503_RegDirA = 0x03; // register address
+static const uint8_t SX_1503_RegDataB = 0x00; // register address Pin State (0: LOW, 1: HIGH)
+static const uint8_t SX_1503_RegDataA = 0x01; // register address Pin State
+static const uint8_t SX_1503_RegDirB = 0x02; // register address Pin Direction (0: output, 1: Input)
+static const uint8_t SX_1503_RegDirA = 0x03; // register address Pin Direction
+static const uint8_t SX_1503_PullUpB = 0x04; // register address Pull Ups (0: pull up disabled, 1: pull up enabled)
+static const uint8_t SX_1503_PullUpA = 0x05; // register address Pull Ups
+static const uint8_t SX_1503_PullDownB = 0x06; // register address Pull Ups (0: pull up disabled, 1: pull up enabled)
+static const uint8_t SX_1503_PullDownA = 0x07; // register address Pull Ups
+static const uint8_t SX_1503_InterruptMaskB = 0x08; // register address Interrupt Mask (0: mask enabled, 1: mask disabled)
+static const uint8_t SX_1503_InterruptMaskA = 0x09; // register address Interrupt Mask
+
+static const uint8_t SX_1503_RegSenseHighB = 0x0A; // register address Interrupt Sense (00: none, 01: rising, 10: falling, 11: both)
+static const uint8_t SX_1503_RegSenseHighA = 0x0B; // register address Interrupt Sense
+static const uint8_t SX_1503_RegSenseLowB = 0x0C; // register address Interrupt Sense
+static const uint8_t SX_1503_RegSenseLowA = 0x0D; // register address Interrupt Sense
+
+static const uint8_t SX_1503_RegInterruptSourceB = 0x0E; // register address Interrupt Source (0: not the source of interrupt, 1: source of interrupt)
+static const uint8_t SX_1503_RegInterruptSourceA = 0x0F; // register address Interrupt Source
+
+static const uint8_t SX_1503_RegEventStatusB = 0x10; // register address Event status of all IOs (0: No event has occured on this IO, 1: An event has occured on this IO)
+static const uint8_t SX_1503_RegEventStatusA = 0x11; // register address Event status of all IOs
 
 // Port Expander outputs:
-static const uint16_t HMI_LED_WDA	= 0b0000001000000000; // Week Day Alarm LED
-static const uint16_t HMI_LED_OT 	= 0b0000000100000000; // One Time Alarm LED
-static const uint16_t HMI_LED_TIME_DATE	= 0b0000000010000000; // Time/Date LED
-static const uint16_t HMI_LED_TIMER1	= 0b0000000001000000; // Timer1 LED /TODO swap back (see findings List PT1)
-static const uint16_t HMI_LED_TIMER2	= 0b0000000000100000; // Timer2 LED /TODO swap back (see findings List PT1)
+static const uint16_t HMI_LED_WDA = 0b0000001000000000; // Week Day Alarm LED
+static const uint16_t HMI_LED_OTA = 0b0000000100000000; // One Time Alarm LED
+static const uint16_t HMI_LED_TIME_DATE = 0b0000000010000000; // Time/Date LED
+static const uint16_t HMI_LED_TIMER1 = 0b0000000001000000; // Timer1 LED /TODO swap back (see findings List PT1)
+static const uint16_t HMI_LED_TIMER2 = 0b0000000000100000; // Timer2 LED /TODO swap back (see findings List PT1)
 
-// TODO uint8_t HMI_BANKA_Buffer[1]	= {0x00}; // Output Buffer of Bank A
-// TODO uint8_t HMI_BANKB_Buffer[1]	= {0x00}; // Output Buffer of Bank B
+static const uint16_t HMI_BTN_WDA = 0b0000000000010000; // Week Day Alarm Button
+static const uint16_t HMI_BTN_OTA = 0b0000000000001000; // One Time Alarm Button
+static const uint16_t HMI_BTN_TIME_DATE = 0b0000000000000100; // Time/Date Button
+static const uint16_t HMI_BTN_TIMER1 = 0b0000000000000010; // Timer1 Button
+static const uint16_t HMI_BTN_TIMER2 = 0b0000000000000001; // Timer2 Button
+static const uint16_t HMI_BTN_ENCODER = 0b0000010000000000; // Encoder Button
 
 
 
--- SW/LCD_Timer_Clock/Core/Src/SX1503.c
+++ SW/LCD_Timer_Clock/Core/Src/SX1503.c
@@ -21,19 +21,83 @@ void HMI_Setup(SX1503 *mySX1503, I2C_HandleTypeDef *I2C_Handle) {
 
 // TODO set default config
 HAL_StatusTypeDef HMI_defaultConfig(SX1503 *mySX1503) {
-	uint8_t buf[12]; // transmission buffer
+	uint8_t buf[14]; // transmission buffer
 
-	// set Bank A outputs (I/O5, I/O6, I/O7)
-	buf[0] = SX_1503_RegDirA;
-	buf[1] = 0b00011111;
-	HAL_I2C_Master_Transmit(mySX1503->I2C_Handle, mySX1503->I2C_ADDRESS, buf, 2,
-			HAL_TIMEOUT);
+	// TODO can be speed up by writing consecutive
 
-	// set Bank B outputs (I/O8, I/O9)
-	buf[0] = SX_1503_RegDirB;
-	buf[1] = 0b11111100;
-	HAL_I2C_Master_Transmit(mySX1503->I2C_Handle, mySX1503->I2C_ADDRESS, buf, 2,
-			HAL_TIMEOUT);
+	// Set Inputs and Outputs
+
+	// set Bank A outputs (I/O5, I/O6, I/O7) (0: output, 1: Input)
+	buf[SX_1503_RegDirA] = 0b00011111;
+	//HAL_I2C_Mem_Write(&hi2c2, SX1503_ADDR, SX_1503_RegDirA, 1, &buf[0], 1, HAL_MAX_DELAY);
+
+	// set Bank B outputs (I/O8, I/O9) (0: output, 1: Input)
+	buf[SX_1503_RegDirB] = 0b11111100;
+	//HAL_I2C_Mem_Write(&hi2c2, SX1503_ADDR, SX_1503_RegDirB, 1, &buf[0], 1, HAL_MAX_DELAY);
+
+	// activate Pull-up resistors for buttons
+
+	// activate pull-ups for Bank A inputs (I/O0, I/O1, I/O2, I/O3, I/O4) (0: pull up disabled, 1: pull up enabled)
+	buf[SX_1503_PullUpA] = 0b00011111;
+	//HAL_I2C_Mem_Write(&hi2c2, SX1503_ADDR, SX_1503_PullUpA, 1, &buf[0], 1, HAL_MAX_DELAY);
+
+	// activate pull-ups for Bank B inputs (I/10) (0: pull up disabled, 1: pull up enabled)
+	buf[SX_1503_PullUpB] = 0b11111100;
+	//HAL_I2C_Mem_Write(&hi2c2, SX1503_ADDR, SX_1503_PullUpB, 1, &buf[0], 1, HAL_MAX_DELAY);
+
+	// deactivate pull-downs for Bank A inputs (0: pull down disabled, 1: pull downs enabled)
+	buf[SX_1503_PullDownA] = 0b00000000;
+
+	// deactivate pull-downs for Bank B inputs (0: pull down disabled, 1: pull downs enabled)
+	buf[SX_1503_PullDownB] = 0b00000000;
+
+	// activate Interrupt Mask
+	uint16_t interruptMask = 0x0000; // all disabled
+	interruptMask |= HMI_BTN_WDA;
+	interruptMask |= HMI_BTN_OTA;
+	interruptMask |= HMI_BTN_TIME_DATE;
+	interruptMask |= HMI_BTN_TIMER1;
+	interruptMask |= HMI_BTN_TIMER2;
+	interruptMask |= HMI_BTN_ENCODER;
+
+	// invert mask to fit register
+	interruptMask = ~interruptMask;
+
+	// set interrupt mask of Bank A
+	buf[SX_1503_InterruptMaskA] = interruptMask; // lower byte
+
+	// set interrupt mask of Bank B
+	buf[SX_1503_InterruptMaskB] = (interruptMask >> 8); // upper byte
+
+	// set Interrupt detection of buttons to falling edge of Bank A (00: none, 01: rising, 10: falling, 11: both)
+	uint16_t SensePattern = 0x0000; // all none
+
+	SensePattern |= 0b10; 		// I/00  falling (HMI_BTN_TIMER2)
+	SensePattern |= 0b10 << 2;	// I/01  falling (HMI_BTN_TIMER1)
+	SensePattern |= 0b10 << 4;	// I/02  falling (HMI_BTN_TIME_DATE)
+	SensePattern |= 0b10 << 6;	// I/03  falling (HMI_BTN_OTA)
+	SensePattern |= 0b10 << 8;	// I/04  falling (HMI_BTN_WDA)
+
+	// set interrupt detection pattern of Bank A, Lower byte
+	buf[SX_1503_RegSenseLowA] = SensePattern; // lower byte
+
+	// set interrupt detection pattern of Bank A, Upper byte
+	buf[SX_1503_RegSenseHighA] = (SensePattern >> 8); // upper byte
+
+	// set Interrupt detection of buttons to falling edge of Bank B (00: none, 01: rising, 10: falling, 11: both)
+	SensePattern = 0x0000; // all none
+	SensePattern |= 0b10 << 4;	// I/010 falling (HMI_BTN_ENCODER)
+
+	// set interrupt detection pattern of Bank B, Lower byte
+	buf[SX_1503_RegSenseLowB] = SensePattern; // lower byte
+
+	// set interrupt detection pattern of Bank B, Upper byte
+	buf[SX_1503_RegSenseHighB] = (SensePattern >> 8); // upper byte
+
+	// Send data packet, beginning with register 0x02 (SX_1503_RegDirB)
+	HAL_I2C_Mem_Write(mySX1503->I2C_Handle, mySX1503->I2C_ADDRESS, SX_1503_RegDirB, 1,
+			&buf[SX_1503_RegDirB], 12, HAL_MAX_DELAY);
+
 }
 
 /*
