566b8456a1bb58a858f0537fa79704e0836cdf31
mariuste <mariustetard@googlemail.com>
2022-11-22 13:40:05 +0100

    adjst resolution of LED brightness

--- SW/LCD_Timer_Clock/Core/Src/main.c
+++ SW/LCD_Timer_Clock/Core/Src/main.c
@@ -2680,7 +2680,7 @@ static void MX_TIM2_Init(void)
   htim2.Instance = TIM2;
   htim2.Init.Prescaler = 10-1;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 100-1;
+  htim2.Init.Period = 255-1;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_PWM_Init(&htim2) != HAL_OK)
